ee63a919421b850d1d638a81db21192aa09c7e60
Bryan Ruelas <bryanruelas09@gmail.com>
2023-08-16 12:36:47 -0700

    iconos y estilos agregados

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+
+module.exports = {
+theme: {
+    extend: {
+    fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+  // ...
+}
